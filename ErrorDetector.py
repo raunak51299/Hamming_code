cff349244883b3d4020276455b9dc170d77efc7c
Raunak <raunak51299@gmail.com>
2021-03-21 22:41:22 +0530

    Update ErrorDetector.py
    Optimized detection algorithm

--- ErrorDetector.py
+++ ErrorDetector.py
@@ -1,55 +1,42 @@
+from functools import reduce
 def main():
 
     print('Enter the hamming code received')
     d=input()
-    data=list(d)
-    data.reverse()
-    c,ch,j,r,error,h,parity_list,h_copy=0,0,0,0,0,[],[],[]
+    m = len(str(d))
+    nr=0
+    for i in range(m): 
+        if(2**i >= m + i + 1): 
+            nr = i
 
-    for k in range(0,len(data)):
-        p=(2**c)
-        h.append(int(data[k]))
-        h_copy.append(data[k])
-        if(p==(k+1)):
-            c=c+1
 
-    for parity in range(0,(len(h))):
-        ph=(2**ch)
-        if(ph==(parity+1)):
+    d = [int(i) for i in str(d)]
+    n = len(d) 
+    res = 0
+  
+    # Calculate parity bits again 
+    for i in range(nr): 
+        val = 0
+        for j in range(1, n + 1): 
+            if(j & (2**i) == (2**i)): 
+                val = val ^ int(d[-1 * j]) 
+        # Create a binary no by appending 
+        # parity bits together. 
+        res = res + val*(10**i) 
+    # Convert binary to decimal 
+    position = int(str(res), 2)# bin to decimal
+    print("position of error is ",position," from right")
 
-            startIndex=ph-1
-            i=startIndex
-            toXor=[]
-
-            while(i<len(h)):
-                block=h[i:i+ph]
-                toXor.extend(block)
-                i+=2*ph
-
-            for z in range(1,len(toXor)):
-                h[startIndex]=h[startIndex]^toXor[z]
-            parity_list.append(h[parity])
-            ch+=1
-    parity_list.reverse()
-    error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
-
-    if((error)==0):
-        print('There is no error in the hamming code received')
-
-    elif((error)>=len(h_copy)):
-        print('Error cannot be detected')
-
+    position = m-position
+    if(d[position] == 1):#correction of error
+        d[position] = 0
     else:
-        print('Error is in',error,'bit')
+        d[position] = 1 
 
-        if(h_copy[error-1]=='0'):
-            h_copy[error-1]='1'
+    string_ints = [str(int) for int in d]
+    d = "".join(string_ints)
 
-        elif(h_copy[error-1]=='1'):
-            h_copy[error-1]='0'
-            print('After correction hamming code is:- ')
-        h_copy.reverse()
-        print(int(''.join(map(str, h_copy))))
+    print("corrected code is ","".join(str(d)))
 
 if __name__ == '__main__':
     main()
